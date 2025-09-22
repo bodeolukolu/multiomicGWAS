5acc2faed33de4373c97a1414989855fd39f6208
bodeolukolu <bolukolu@utk.edu>
2025-09-22 10:40:50 -0400

    Initial commit

--- run_parameters_multiomicGWAS.R
+++ run_parameters_multiomicGWAS.R
@@ -1,5 +1,20 @@
 #!/usr/bin/env Rscript
 
+# # List of required packages
+# pkgs <- c("AGHmatrix", "qqplotr", "ggplot2", "dplyr", "data.table", "stringr",
+#           "heatmaply", "ppcor", "zoo", "GGally", "reshape2", "compositions", "sommer", "mice", "qvalue")
+# # Install missing packages
+# for (p in pkgs) {
+#   if (!requireNamespace(p, quietly = TRUE)) {
+#     message("Installing package: ", p)
+#     install.packages(p, repos = "https://cloud.r-project.org")
+#   }
+# }
+# install.packages("remotes")
+# library("remotes")
+# remotes::install_github("jendelman/GWASpoly")
+
+
 source("https://github.com/bodeolukolu/multiomicGWAS/raw/refs/heads/main/multiomicGWAS.R")
 
 multiomicGWAS <- (
